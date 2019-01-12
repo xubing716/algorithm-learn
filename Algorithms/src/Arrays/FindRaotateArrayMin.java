11196157b76e409169bd26552038833449e6a2b9
xubing716 <1608070045@qq.com>
2019-01-12 23:23:07 +0800

    finish array,next start learn tree

--- Algorithms/src/Arrays/FindRaotateArrayMin.java
+++ Algorithms/src/Arrays/FindRaotateArrayMin.java
@@ -27,9 +27,9 @@ public class FindRaotateArrayMin {
 
         return arr[left];
     }
-    
+
     public static void main(String[] args) {
-        int [] arr = {4,5,6,7,8,9,0,1,2,3};
+        int[] arr = {4, 5, 6, 7, 8, 9, 0, 1, 2, 3};
         System.out.println(findRotateArrayMin(arr));
     }
 }
